14970b0aeccd44557af7ab04ed5b7fc9a634b238
frehkxu <frehaiku@users.noreply.github.com>
2022-10-13 20:27:24 +0800

    feat: add Password component (#24)

---
+++ docs/demos/guide/password/json-schema.vue
@@ -0,0 +1,36 @@
+<template>
+  <FormProvider :form="form">
+    <SchemaField :schema="schema" />
+    <Submit @submit="onSubmit">提交</Submit>
+  </FormProvider>
+</template>
+
+<script lang="ts" setup>
+import { createForm } from '@formily/core'
+import { createSchemaField, FormProvider } from '@formily/vue'
+import { FormItem, Password, Submit } from '@formily/element-plus'
+
+const schema = {
+  type: 'object',
+  properties: {
+    password: {
+      type: 'string',
+      title: '密码框',
+      'x-decorator': 'FormItem',
+      'x-component': 'Password',
+    },
+  },
+}
+
+const form = createForm()
+const { SchemaField } = createSchemaField({
+  components: {
+    FormItem,
+    Password,
+  },
+})
+
+const onSubmit = (value) => {
+  console.log(value)
+}
+</script>
---
+++ docs/demos/guide/password/template.vue
@@ -0,0 +1,23 @@
+<template>
+  <FormProvider :form="form">
+    <Field
+      name="password"
+      title="密码框"
+      :decorator="[FormItem]"
+      :component="[Password]"
+    />
+    <Submit @submit="log">提交</Submit>
+  </FormProvider>
+</template>
+
+<script lang="ts" setup>
+import { createForm } from '@formily/core'
+import { FormProvider, Field } from '@formily/vue'
+import { FormItem, Password, Submit } from '@formily/element-plus'
+
+const form = createForm()
+
+const log = (value) => {
+  console.log(value)
+}
+</script>
