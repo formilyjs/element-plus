7d74ea9e88bb389c3c687f0c67586176ddeac290
Summer <SorrowX@users.noreply.github.com>
2024-07-01 13:38:14 +0800

    fix: switch v-model invalid in high version element-plus (#47)

--- packages/components/src/switch/index.ts
+++ packages/components/src/switch/index.ts
@@ -1,9 +1,19 @@
+import { transformComponent } from '../__builtins__'
 import { connect, mapProps } from '@formily/vue'
-
 import { ElSwitch } from 'element-plus'
 
 export type SwitchProps = typeof ElSwitch
 
-export const Switch = connect(ElSwitch, mapProps({ readOnly: 'readonly' }))
+const TransformElSwitch = transformComponent<SwitchProps>(ElSwitch, {
+  change: 'update:modelValue',
+})
+
+export const Switch = connect(
+  TransformElSwitch,
+  mapProps({
+    value: 'modelValue',
+    readOnly: 'readonly',
+  })
+)
 
 export default Switch
